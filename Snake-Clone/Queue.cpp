ec0f7068c24cd9ab3f24fbcb4dd1243271530aec
Leandro Novak <leandro.novak@outlook.com>
2016-10-25 00:22:03 -0200

    Update Queue.cpp

--- Snake-Clone/Queue.cpp
+++ Snake-Clone/Queue.cpp
@@ -9,38 +9,31 @@ Queue::Queue()
 
 void Queue::push(int direction)
 {
-	if (first == nullptr)
-	{
+	if (first == nullptr) {
 		first = new Node;
 		first->img.load("bd", 375, 275);
 		first->next = nullptr;
 	}
-	else
-	{
+	else {
 		pushAux(first, direction);
 	}
 }
 
 void Queue::pushAux(Node* node, int direction)
 {
-	if (node->next == nullptr)
-	{
+	if (node->next == nullptr) {
 		Node* tnode = new Node;
 		
-		if (direction == Direction::up)
-		{
+		if (direction == Direction::up) {
 			tnode->img.load("bd", node->img.getPosition().x, node->img.getPosition().y - 25);
 		}
-		else if (direction == Direction::down)
-		{
+		else if (direction == Direction::down) {
 			tnode->img.load("bd", node->img.getPosition().x, node->img.getPosition().y + 25);
 		}
-		else if (direction == Direction::left)
-		{
+		else if (direction == Direction::left) {
 			tnode->img.load("bd", node->img.getPosition().x - 25, node->img.getPosition().y);
 		}
-		else if (direction == Direction::right)
-		{
+		else if (direction == Direction::right) {
 			tnode->img.load("bd", node->img.getPosition().x + 25, node->img.getPosition().y);
 		}
 
@@ -48,24 +41,20 @@ void Queue::pushAux(Node* node, int direction)
 		node->next = tnode;
 		queueSize++;
 	}
-	else
-	{
+	else {
 		pushAux(node->next, direction);
 	}
 }
 
 void Queue::pop()
 {
-	if (first != nullptr)
-	{
-		if (first->next != nullptr)
-		{
+	if (first != nullptr) {
+		if (first->next != nullptr) {
 			Node* tempPtr = first->next;
 			delete first;
 			first = tempPtr;
 		}
-		else
-		{
+		else {
 			delete first;
 		}
 		queueSize--;
@@ -130,27 +119,21 @@ bool Queue::checkCollision()
 
 void Queue::update(float gameTime, int direction)
 {
-	if (!empty())
-	{
+	if (!empty()) {
 		sf::Vector2f tempPos = first->img.getPosition();
-		if (direction == Direction::up)
-		{
+		if (direction == Direction::up) {
 			first->img.setPosition(first->img.getPosition().x, first->img.getPosition().y - 25);
 		}
-		else if (direction == Direction::down)
-		{
+		else if (direction == Direction::down) {
 			first->img.setPosition(first->img.getPosition().x, first->img.getPosition().y + 25);
 		}
-		else if (direction == Direction::left)
-		{
+		else if (direction == Direction::left) {
 			first->img.setPosition(first->img.getPosition().x - 25, first->img.getPosition().y);
 		}
-		else if (direction == Direction::right)
-		{
+		else if (direction == Direction::right) {
 			first->img.setPosition(first->img.getPosition().x + 25, first->img.getPosition().y);
 		}
-		if (first->next != nullptr)
-		{
+		if (first->next != nullptr) {
 			recursiveUpdate(first->next, tempPos);
 		}
 	}
@@ -160,16 +143,14 @@ void Queue::recursiveUpdate(Node * node, sf::Vector2f lastPos)
 {
 	sf::Vector2f tempPos2 = node->img.getPosition();
 	node->img.setPosition(lastPos);
-	if (node->next != nullptr)
-	{
+	if (node->next != nullptr) {
 		recursiveUpdate(node->next, tempPos2);
 	}
 }
 
 void Queue::draw(sf::RenderTarget & target)
 {
-	if (!empty())
-	{
+	if (!empty()) {
 		Node* aux = first;
 		aux->img.draw(target);
 		while (aux->next != nullptr)
