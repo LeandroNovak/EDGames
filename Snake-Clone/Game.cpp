1380fc8dd31cee75fef4880087013b13ebc5057c
Leandro Novak <leandro.novak@outlook.com>
2016-10-25 00:19:29 -0200

    Update Game.cpp

--- Snake-Clone/Game.cpp
+++ Snake-Clone/Game.cpp
@@ -5,13 +5,13 @@
 #include "Game.h"
 
 // Construtor da classe Game
-// Recebe por parâmetro sf::VideoMode com largura e altura da janela de jogo,
+// Recebe por parÃ¢metro sf::VideoMode com largura e altura da janela de jogo,
 // uma string contendo o nome da janela e sf::Style o estilo da janela.
 
 Game::Game() : mainWindow(sf::VideoMode(800, 600), "EDGame", sf::Style::Close)
 {
-	mainWindow.setFramerateLimit(5);						// Força a taxa de frames limite do jogo
-	mainWindow.setVerticalSyncEnabled(false);				// Desativa a sincronização Vertical
+	mainWindow.setFramerateLimit(5);						// ForÃ§a a taxa de frames limite do jogo
+	mainWindow.setVerticalSyncEnabled(false);				// Desativa a sincronizaÃ§Ã£o Vertical
 	
 	currentState = State::menu;
 	currentDirection = Direction::right;
@@ -54,8 +54,7 @@ void Game::run()
 	{
 		timeSinceLastUpdate += clock.restart();
 		
-		if (timeSinceLastUpdate > gameTime)
-		{
+		if (timeSinceLastUpdate > gameTime) {
 			timeSinceLastUpdate -= gameTime;
 			eventHandler();
 			update(gameTime.asSeconds());
@@ -73,8 +72,7 @@ void Game::eventHandler()
 	while (mainWindow.pollEvent(event))
 	{
 		// Eventos verificados no menu do jogo
-		if (currentState == State::menu)
-		{
+		if (currentState == State::menu) {
 			switch (event.type)
 			{
 			case sf::Event::Closed:
@@ -82,34 +80,28 @@ void Game::eventHandler()
 				break;
 
 			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Key::Escape)
-				{
+				if (event.key.code == sf::Keyboard::Key::Escape) {
 					mainWindow.close();
 					break;
 				}
-				if (event.key.code == sf::Keyboard::Key::Up)
-				{
+				if (event.key.code == sf::Keyboard::Key::Up) {
 					newGameButton.setFocus(true);
 					exitButton.setFocus(false);
 					currentButton = 0;
 					break;
 				}
-				if (event.key.code == sf::Keyboard::Key::Down)
-				{
+				if (event.key.code == sf::Keyboard::Key::Down) {
 					newGameButton.setFocus(false);
 					exitButton.setFocus(true);
 					currentButton = 1;
 					break;
 				}
-				if (event.key.code == sf::Keyboard::Key::End || event.key.code == sf::Keyboard::Key::Space)
-				{
-					if (currentButton == 0)
-					{
+				if (event.key.code == sf::Keyboard::Key::End || event.key.code == sf::Keyboard::Key::Space) {
+					if (currentButton == 0) {
 						currentState = State::playing;
 						break;
 					}
-					if (currentButton == 1)
-					{
+					if (currentButton == 1) {
 						mainWindow.close();
 						break;
 					}				
@@ -127,28 +119,23 @@ void Game::eventHandler()
 				break;
 
 			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Key::Escape)
-				{
+				if (event.key.code == sf::Keyboard::Key::Escape) {
 					mainWindow.close();
 					break;
 				}
-				else if (event.key.code == sf::Keyboard::Key::Up && currentDirection != down)
-				{
+				else if (event.key.code == sf::Keyboard::Key::Up && currentDirection != down) {
 					currentDirection = Direction::up;
 					break;
 				}
-				else if (event.key.code == sf::Keyboard::Key::Down && currentDirection != up)
-				{
+				else if (event.key.code == sf::Keyboard::Key::Down && currentDirection != up) {
 					currentDirection = Direction::down;
 					break;
 				}
-				else if (event.key.code == sf::Keyboard::Key::Left && currentDirection != right)
-				{
+				else if (event.key.code == sf::Keyboard::Key::Left && currentDirection != right) {
 					currentDirection = Direction::left;
 					break;
 				}
-				else if (event.key.code == sf::Keyboard::Key::Right && currentDirection != left)
-				{
+				else if (event.key.code == sf::Keyboard::Key::Right && currentDirection != left) {
 					currentDirection = Direction::right;
 					break;
 				}
@@ -158,20 +145,18 @@ void Game::eventHandler()
 	
 		if (currentState == State::lose)
 		{
-		switch (event.type)
+			switch (event.type)
 			{
 			case sf::Event::Closed:
 				mainWindow.close();
 				break;
 
 			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Key::Escape)
-				{
+				if (event.key.code == sf::Keyboard::Key::Escape) {
 					mainWindow.close();
 					break;
 				}
-				else if (event.key.code == sf::Keyboard::Key::Space)
-				{
+				else if (event.key.code == sf::Keyboard::Key::Space) {
 					mainWindow.close();
 					break;
 				}
@@ -180,30 +165,26 @@ void Game::eventHandler()
 	}
 }
 
-// Atualiza conteúdo do jogo
+// Atualiza conteÃºdo do jogo
 void Game::update(float timeAsSeconds)
 {
 	sf::Mouse mouse;
 
-	if (currentState == State::playing)
-	{
+	if (currentState == State::playing) {
 		snake.update(timeAsSeconds, currentDirection);
-		if (snake.checkFood(food.getPosition()))
-		{
+		if (snake.checkFood(food.getPosition())) {
 			food.load("fd", (rand() % 31) * 25, (rand() % 24) * 25);
 			score++;
 			std::string temp = std::to_string(score);
 			textScore.setString("Score: " + (sf::String)temp);
 			snake.push(currentDirection);
 		}
-		if (snake.checkAutokill())
-		{
+		if (snake.checkAutokill()) {
 			textScore.setPosition(800/2 - 150, 600/2 + 50);
 			textScore.setString("(press space to exit)");
 			currentState = State::lose;
 		}
-		if (snake.checkCollision())
-		{
+		if (snake.checkCollision()) {
 			textScore.setPosition(800/2 - 150, 600/2 + 50);
 			textScore.setString("(press space to exit)");
 			currentState = State::lose;
@@ -211,23 +192,21 @@ void Game::update(float timeAsSeconds)
 	}
 }
 
-// Exibe conteúdo na janela do jogo
+// Exibe conteÃºdo na janela do jogo
 void Game::draw()
 {
-	// Conteúdo a ser desenhado durante o menu
+	// ConteÃºdo a ser desenhado durante o menu
 	mainWindow.clear(sf::Color::White);
 
-	if (currentState == State::menu)
-	{
+	if (currentState == State::menu) {
 		background.draw(mainWindow);
 		title.draw(mainWindow);
 		newGameButton.draw(mainWindow);
 		exitButton.draw(mainWindow);
 	}
 
-	// Conteúdo a ser desenhado durante a fase
-	if (currentState == State::playing)
-	{
+	// ConteÃºdo a ser desenhado durante a fase
+	if (currentState == State::playing) {
 		background.draw(mainWindow);
 		edges.draw(mainWindow);
 		snake.draw(mainWindow);
@@ -236,8 +215,7 @@ void Game::draw()
 		sound.play();
 	}
 
-	if (currentState == State::lose)
-	{
+	if (currentState == State::lose) {
 		background.draw(mainWindow);
 		gameOverScreen.draw(mainWindow);
 		mainWindow.draw(textScore);
