0249cf840742b75a4c3b9518cf96e9a6aa39abdf
Leandro Novak <leandro.novak@outlook.com>
2016-10-28 23:11:52 -0200

    Update main.cpp

--- Snake-Clone/main.cpp
+++ Snake-Clone/main.cpp
@@ -1,10 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include "Game.h"
-
 /******************************************
  * Função main do jogo, cria um objeto do 
  * tipo Game e chama o loop principal.
 *******************************************/
+
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
 int main()
 {
 	Game game;
